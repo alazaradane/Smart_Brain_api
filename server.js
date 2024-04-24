3449a27578bb235beb4679c5b63b88ccb7b8ad32
alazaradane <alazargithub369@gmail.com>
2024-04-24 06:17:45 +0300

    connected database with backend

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
+
+const postgres = knex({
+    client:'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'',
+        database:'smart_brain'
+    }
+})
+
+console.log(postgres.select('*').from('users'))
+
 
 const app = express()
 app.use(bodyParser.json()) 
